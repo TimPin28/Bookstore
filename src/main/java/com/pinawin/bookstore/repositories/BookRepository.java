f535c198a556212e930ea40d6f2d0b1e4e847a74
TimPin28 <timothyvincentpinawin@gmail.com>
2026-01-28 10:21:47 +0800

    added basic repositories for the initial entities also added a sanity check in properties to see if repository is being used

---
+++ src/main/java/com/pinawin/bookstore/repositories/BookRepository.java
@@ -0,0 +1,11 @@
+package com.pinawin.bookstore.repositories;
+
+import com.pinawin.bookstore.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
+
---
+++ src/main/java/com/pinawin/bookstore/repositories/CartItemRepository.java
@@ -0,0 +1,12 @@
+package com.pinawin.bookstore.repositories;
+
+import com.pinawin.bookstore.models.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+}
+
