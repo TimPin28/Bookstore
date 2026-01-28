dfea2f8e5f767e17bcd3c81ca56a959f38f229cc
TimPin28 <timothyvincentpinawin@gmail.com>
2026-01-28 14:43:22 +0800

    Added BookService and generic methods

---
+++ src/main/java/com/pinawin/bookstore/services/BookService.java
@@ -0,0 +1,26 @@
+package com.pinawin.bookstore.services;
+
+import com.pinawin.bookstore.models.Book;
+import com.pinawin.bookstore.repositories.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public List<Book> searchBooks(String keyword) {
+        return bookRepository.findByTitleContainingIgnoreCase(keyword);
+    }
+}
+
