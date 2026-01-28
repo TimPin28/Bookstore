776d3b776d8b4d4d1fd890b4ccef34daa898c67b
TimPin28 <timothyvincentpinawin@gmail.com>
2026-01-28 15:07:29 +0800

    Added BookController to handle book requests

---
+++ src/main/java/com/pinawin/bookstore/controller/BookController.java
@@ -0,0 +1,30 @@
+package com.pinawin.bookstore.controller;
+
+import com.pinawin.bookstore.models.Book;
+import com.pinawin.bookstore.services.BookService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+@CrossOrigin // allows JS calls from browser
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/search")
+    public List<Book> searchBooks(@RequestParam String keyword) {
+        return bookService.searchBooks(keyword);
+    }
+}
+
