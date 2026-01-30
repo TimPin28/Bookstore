a63c3e79a60f9723f0012e6fe47bf9486ba318fa
TimPin28 <timothyvincentpinawin@gmail.com>
2026-01-30 16:31:51 +0800

    Added Unit Testing of BookService

---
+++ src/test/java/com/pinawin/bookstore/services/BookServiceTest.java
@@ -0,0 +1,17 @@
+package com.pinawin.bookstore.services;
+
+import com.pinawin.bookstore.repositories.BookRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BookServiceTest {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @InjectMocks
+    private BookService bookService;
+}
