5a3cf4cbb128bea8d3467ff6e7f6b3c55bbd0453
TimPin28 <timothyvincentpinawin@gmail.com>
2026-01-30 09:11:11 +0800

    book inventory will be reduced after a successful checkout

--- src/main/java/com/pinawin/bookstore/services/CheckoutService.java
+++ src/main/java/com/pinawin/bookstore/services/CheckoutService.java
@@ -1,9 +1,7 @@
 package com.pinawin.bookstore.services;
 
-import com.pinawin.bookstore.models.CartItem;
-import com.pinawin.bookstore.models.Order;
-import com.pinawin.bookstore.models.OrderItem;
-import com.pinawin.bookstore.models.User;
+import com.pinawin.bookstore.models.*;
+import com.pinawin.bookstore.repositories.BookRepository;
 import com.pinawin.bookstore.repositories.CartItemRepository;
 import com.pinawin.bookstore.repositories.OrderRepository;
 import jakarta.transaction.Transactional;
@@ -17,11 +15,14 @@ public class CheckoutService {
 
     private final CartItemRepository cartItemRepository;
     private final OrderRepository orderRepository;
+    private final BookRepository bookRepository;
 
     public CheckoutService(CartItemRepository cartItemRepository,
-                           OrderRepository orderRepository) {
+                           OrderRepository orderRepository,
+                           BookRepository bookRepository) {
         this.cartItemRepository = cartItemRepository;
         this.orderRepository = orderRepository;
+        this.bookRepository = bookRepository;
     }
 
     @Transactional
@@ -39,16 +40,29 @@ public class CheckoutService {
         BigDecimal total = BigDecimal.ZERO;
 
         for (CartItem cartItem : cartItems) {
+
+            Book book = cartItem.getBook();
+
+            // check book stock
+            if (book.getStock() < cartItem.getQuantity()) {
+                throw new RuntimeException(
+                        "Not enough stock for book: " + book.getTitle()
+                );
+            }
+
+            // reduce stock if there is stock
+            book.setStock(book.getStock() - cartItem.getQuantity());
+
             OrderItem orderItem = new OrderItem();
             orderItem.setOrder(order);
-            orderItem.setBook(cartItem.getBook());
+            orderItem.setBook(book);
             orderItem.setQuantity(cartItem.getQuantity());
-            orderItem.setPrice(cartItem.getBook().getPrice());
+            orderItem.setPrice(book.getPrice());
 
             order.getOrderItems().add(orderItem);
 
             total = total.add(
-                    cartItem.getBook().getPrice()
+                    book.getPrice()
                             .multiply(BigDecimal.valueOf(cartItem.getQuantity()))
             );
         }
@@ -63,4 +77,5 @@ public class CheckoutService {
         return savedOrder;
     }
 }
+
 
